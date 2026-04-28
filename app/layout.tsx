6b9ecb9667d09981377386dd548664752262a96d
Alexander Enright <alexenrightt@gmail.com>
2026-04-28 05:26:55 -0700

    Add Google Analytics tracking (G-911FQLSMWT)

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-911FQLSMWT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-911FQLSMWT');
+            `
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
